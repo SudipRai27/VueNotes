f0b5f57c8224a811d3e31f8042c6e5d0c4a6b3f1
sudipdigitalbasis <sudipdigitalbasis@github.com>
2021-08-02 22:18:09 +1000

    Fixed minor bug

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,24 +10,63 @@ window.Vue = require("vue").default;
 Vue.use(Notifications);
 Vue.component("App", require("./components/App.vue").default);
 
-Vue.prototype.$displayError = function(errMessage) {
+Vue.prototype.$displayError = function (errMessage) {
     this.$notify({
         group: "notification",
         type: "error",
         title: "Error",
         text: errMessage,
-        duration: 6000
+        duration: 6000,
     });
 };
 
-Vue.prototype.$displaySuccess = function(successMessage) {
+Vue.prototype.$displaySuccess = function (successMessage) {
     this.$notify({
         group: "notification",
         type: "success",
         title: "Success",
-        text: successMessage
+        text: successMessage,
     });
 };
+
+axios.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (err) {
+        switch (err.response.status) {
+            case 401:
+                const authenticated = store.getters["auth/authenticated"];
+                if (authenticated) {
+                    store.dispatch("auth/clearAuth").then(() => {
+                        router.push({ name: "login" });
+                    });
+                }
+                Vue.prototype.$displayError(err.response.data.message);
+                return Promise.reject(err);
+            case 422:
+                Vue.prototype.$displayError("Validation error");
+                return Promise.reject(err);
+            case 403:
+                Vue.prototype.$displayError(err.response.data.message);
+                return Promise.reject(err);
+            case 504:
+                Vue.prototype.$displayError("Gateway Timeout");
+                return Promise.reject(err);
+            case 404:
+                Vue.prototype.$displayError(err.response.data.message);
+                return Promise.reject(err);
+            case 400:
+                Vue.prototype.$displayError(err.response.data.message);
+                return Promise.reject(err);
+            case 500:
+                Vue.prototype.$displayError(err.response.data.message);
+                return Promise.reject(err);
+            default:
+                return Promise.reject(err);
+        }
+    }
+);
 
 store
     .dispatch("auth/attempt", localStorage.getItem("authToken"))
@@ -35,46 +74,6 @@ store
         const app = new Vue({
             el: "#app",
             router,
-            store
+            store,
         });
-
-        axios.interceptors.response.use(
-            function(response) {
-                return response;
-            },
-            function(err) {
-                switch (err.response.status) {
-                    case 401:
-                        const authenticated =
-                            store.getters["auth/authenticated"];
-                        if (authenticated) {
-                            store.dispatch("auth/clearAuth").then(() => {
-                                router.push({ name: "login" });
-                            });
-                        }
-                        app.$displayError(err.response.data.message);
-                        return Promise.reject(err);
-                    case 422:
-                        app.$displayError("Validation error");
-                        return Promise.reject(err);
-                    case 403:
-                        app.$displayError(err.response.data.message);
-                        return Promise.reject(err);
-                    case 504:
-                        app.$displayError("Gateway Timeout");
-                        return Promise.reject(err);
-                    case 404:
-                        app.$displayError(err.response.data.message);
-                        return Promise.reject(err);
-                    case 400:
-                        app.$displayError(err.response.data.message);
-                        return Promise.reject(err);
-                    case 500:
-                        app.$displayError(err.response.data.message);
-                        return Promise.reject(err);
-                    default:
-                        return Promise.reject(err);
-                }
-            }
-        );
     });
